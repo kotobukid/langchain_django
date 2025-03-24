f0ca447a3ef338b7893f912f3afc755da669ccc3
kotobukid <kotobuki@daqua.jp>
2025-03-24 12:57:31 +0900

    tiktokenを使用し、トークン数を事前に確認する

--- webapp/core/views.py
+++ webapp/core/views.py
@@ -1,3 +1,32 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+import tiktoken
 
-# Create your views here.
+
+def tiktoken_form(request):
+    body = request.session.get('body') or ''
+    tokens = request.session.get('tokens') or 0
+
+    request.session.flush()
+
+    template = 'tiktoken_form.html'
+
+    return render(request, template, {
+        "body": body,
+        "tokens": tokens,
+    })
+
+
+def process_tiktoken(request):
+    body = request.POST.get('body').strip()
+    if not body:
+        raise '403'
+
+    encoding = tiktoken.encoding_for_model("gpt-4o")
+    tokens = encoding.encode(body)
+
+    print(tokens)
+    request.session['body'] = body
+    request.session['tokens'] = len(tokens)
+
+    return HttpResponseRedirect('/')
--- webapp/webapp/urls.py
+++ webapp/webapp/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core import views
 
 urlpatterns = [
+    path('', views.tiktoken_form),
+    path('tiktoken', views.process_tiktoken),
     path('admin/', admin.site.urls),
 ]
