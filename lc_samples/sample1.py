e702189302373e4cfdf5a63a59d33eda5e6f1620
kotobukid <kotobuki@daqua.jp>
2025-03-25 14:59:17 +0900

    JSONモードを使用し、更にPydanticでクラスのインスタンス化

--- lc_samples/sample1.py
+++ lc_samples/sample1.py
@@ -1,20 +1,18 @@
-import os
 from dotenv import load_dotenv
-from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
-from langchain_core.prompts import PromptTemplate, ChatPromptTemplate, MessagesPlaceholder
-from langchain_core.output_parsers import PydanticOutputParser, StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
 from langchain_openai import ChatOpenAI
 from sample2 import Recipe
 
 load_dotenv(dotenv_path="../.env")
-api_key = os.environ.get("OPENAI_API_KEY")
-
 
 def main(dish: str):
-    model = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+    llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+    llm.bind(
+        response_format={"type": "json_object"}
+    )
 
     output_parser = PydanticOutputParser(pydantic_object=Recipe)
-    str_output_parser = StrOutputParser()
     format_instructions = output_parser.get_format_instructions()
 
     prompt = ChatPromptTemplate.from_messages(
@@ -28,11 +26,12 @@ def main(dish: str):
         format_instructions=format_instructions
     )
 
-    chain = prompt_with_format_instructions | model | str_output_parser
+    chain = prompt_with_format_instructions | llm | output_parser
 
     ai_message = chain.invoke({"dish": dish})
 
     print(ai_message)
 
+
 if __name__ == "__main__":
-    main("冷や汁")
+    main("コロッケ")
