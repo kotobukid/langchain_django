4d44b0a073740d62144e57bd882b70edce5154fd
kotobukid <kotobuki@daqua.jp>
2025-03-25 13:31:31 +0900

    サンプル環境ファイルとOpenAI API対応を追加
    .env.sampleを追加し、OpenAI APIキーの管理方法を導入しました。また、python-dotenvやlangchain-openaiをrequirements.txtに追加し、新しい機能に対応しました。サンプルコードではAPIキーを用いたモデルの利用例を実装しました。

--- lc_samples/sample1.py
+++ lc_samples/sample1.py
@@ -1,13 +1,22 @@
+import os
+from dotenv import load_dotenv
 from langchain_core.messages import SystemMessage, HumanMessage
+from langchain_openai import ChatOpenAI
+
+load_dotenv(dotenv_path="../.env")
+api_key = os.environ.get("OPENAI_API_KEY")
 
 
 def main():
+    model = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+
     messages = [
         SystemMessage("You are a helpful assistant."),
         HumanMessage("こんにちは")
     ]
 
-    print(messages)
+    for chunk in model.stream(messages):
+        print(chunk.content, end="", flush=True)
 
 
 if __name__ == "__main__":
