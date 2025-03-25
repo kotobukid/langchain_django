cbbfd0fd4532855c042e1d4754d1f1bd22505915
kotobukid <kotobuki@daqua.jp>
2025-03-25 13:36:05 +0900

    PromptTemplateの基本的な用例

--- lc_samples/sample1.py
+++ lc_samples/sample1.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from langchain_core.messages import SystemMessage, HumanMessage
+from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
 
 load_dotenv(dotenv_path="../.env")
@@ -9,14 +10,21 @@ api_key = os.environ.get("OPENAI_API_KEY")
 
 def main():
     model = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+    prompt = PromptTemplate.from_template("""以下の料理のレシピを教えて下さい。
+    
+    料理名: {dish}""")
 
-    messages = [
-        SystemMessage("You are a helpful assistant."),
-        HumanMessage("こんにちは")
-    ]
+    prompt_value = prompt.invoke({"dish": "カレー"})
 
-    for chunk in model.stream(messages):
-        print(chunk.content, end="", flush=True)
+    print(prompt_value.text)
+
+    # messages = [
+    #     SystemMessage("You are a helpful assistant."),
+    #     HumanMessage("こんにちは")
+    # ]
+    #
+    # for chunk in model.stream(messages):
+    #     print(chunk.content, end="", flush=True)
 
 
 if __name__ == "__main__":
