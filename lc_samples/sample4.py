c072f66f155f3ef057377e053b67c5d5e191877d
kotobukid <kotobuki@daqua.jp>
2025-03-26 14:01:42 +0900

    呼び出し

--- lc_samples/sample4.py
+++ lc_samples/sample4.py
@@ -1,19 +1,32 @@
-from langchain_core.prompts import ChatPromptTemplate, HumanMessagePromptTemplate, SystemMessagePromptTemplate
+from dotenv import load_dotenv
+from langchain_core.prompts import ChatPromptTemplate, HumanMessagePromptTemplate, SystemMessagePromptTemplate, AIMessagePromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+from langchain_openai import ChatOpenAI
+
+load_dotenv(dotenv_path="../.env")
 
 # Create a HumanMessagePromptTemplate directly from the template string
 template_string = "Hello {name}"
 human_message_template = HumanMessagePromptTemplate.from_template(template_string)
 
-system_message_template = SystemMessagePromptTemplate.from_template("You are a helpful assistant.")
-
+system_message = SystemMessagePromptTemplate.from_template("You are a helpful assistant. answer in japanese.")
+ai_message = AIMessagePromptTemplate.from_template("こんにちは {name}! 本日はいかがしますか？")
 
 # Create the ChatPromptTemplate
 prompts = ChatPromptTemplate.from_messages([
-    system_message_template,
+    system_message,
     human_message_template,
+    ai_message,
+    ("human", "How to lean Rust lang")
 ])
 
+llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+
+output_parser = StrOutputParser()
+
 # Invoke the template with the required variables
-texts = prompts.invoke({"name": "John"})
+texts = (prompts | llm | output_parser).invoke({"name": "John"})
+
 
 print(texts)
